b19b5f0ae9e4c3a61b7d934b4679280215f02a66
Hayat Abdulfetah <hayatabdulfetah7@gmail.com>
2026-09-13 00:41:41 +0300

    Update API URL to production endpoint

--- frontend/app.js
+++ frontend/app.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:5000/api/tasks";
+const API_URL = "https://task-manager-api-8amx.onrender.com/api/tasks";
 const taskList = document.getElementById("task-list");
 const taskForm = document.getElementById("task-form");
 
